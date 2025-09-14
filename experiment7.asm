477049e83dc3cda91dc49fe9cc4065cd26bd5e37
kapiltrip <kapiltrip.kapil@gmail.com>
2025-09-14 23:47:57 +0530

    experiment 7 fixed

--- experiment7.asm
+++ experiment7.asm
@@ -1,36 +1,57 @@
-        AREA    EXP7, CODE, READONLY
-        EXPORT  _start
+        AREA    PROGRAM, CODE, READONLY
         ENTRY
 
-_start
-        LDR     r0, =DIVIDEND   ;the address of dividend in ro 
-        LDR     r0, [r0]        ;dereferences the pointer stored in r0 and loads , the address data of r0
-        LDR     r1, =DIVISOR
-        LDR     r1, [r1]
-        MOV     r2, #0          ; quotient (how many times, should i divide , i.e the count )
-        CMP     r1, #0
-        BEQ     done            ;this is a reference 1
+MAIN
+        ; Load external addresses from data labels (no '=' immediate)
+        LDR     R1, DIVIDEND_PTR    ; R1 = 0x00003020 (dividend address)
+        LDR     R2, DIVISOR_PTR     ; R2 = 0x00003024 (divisor address)
 
-loop
-        CMP     r0, r1
-        BLT     done             ;this is reference 2 BLT signal -> done is the target 
-        SUB     r0, r0, r1             
-        ADD     r2, r2, #1      ;increasing the quotient 
-        B       loop
+        ; Read operands from memory
+        LDR     R3, [R1]            ; R3 = dividend
+        LDR     R4, [R2]            ; R4 = divisor
+        MOV     R5, #0              ; R5 = quotient
 
-done                            ;loop will start from here, if CMP r1 , #0 will give 1 i.e z flag set
-                                ;here there is the definition of done , which is referred earlier .
-        LDR     r3, =QUOTIENT
-        STR     r2, [r3]
-        LDR     r3, =REMAINDER
-        STR     r0, [r3]
+        ; Handle divide-by-zero: leave quotient=0, remainder=dividend
+        CMP     R4, #0
+        BEQ     STORE_RESULTS
 
-stop
-        B       stop
+LOOP
+        CMP     R3, R4
+        BLT     STORE_RESULTS       ; if dividend < divisor, exit
+        SUB     R3, R3, R4          ; dividend -= divisor
+        ADD     R5, R5, #1          ; quotient++
+        B       LOOP
 
-        ALIGN
-DIVIDEND DCD 20 
-DIVISOR  DCD 3
-QUOTIENT DCD 0
-REMAINDER DCD 0
+STORE_RESULTS
+        ; Write results back to memory so you can view them
+        LDR     R6, QUOTIENT_PTR    ; R6 = 0x00003028 (quotient out)
+        STR     R5, [R6]
+        LDR     R6, REMAINDER_PTR   ; R6 = 0x0000302C (remainder out)
+        STR     R3, [R6]
+
+DONE
+        B       DONE                ; R5=quotient, R3=remainder
+
+        AREA    PROGRAM, DATA, READONLY
+DIVIDEND_PTR   DCD 0x00003020       ; input: dividend address
+DIVISOR_PTR    DCD 0x00003024       ; input: divisor  address
+QUOTIENT_PTR   DCD 0x00003028       ; output: quotient address
+REMAINDER_PTR  DCD 0x0000302C       ; output: remainder address
+        
+        ;
+        ; Example memory contents (big-endian) and binaries
+        ; Inputs you set before running:
+        ;   [0x00003020] dividend = 0x0000012C (300)
+        ;       bytes  : 00 00 01 2C
+        ;       binary : 00000000 00000000 00000001 00101100
+        ;   [0x00003024] divisor  = 0x0000000F (15)
+        ;       bytes  : 00 00 00 0F
+        ;       binary : 00000000 00000000 00000000 00001111
+        ; Outputs written after running:
+        ;   [0x00003028] quotient = 0x00000014 (20)
+        ;       bytes  : 00 00 00 14
+        ;       binary : 00000000 00000000 00000000 00010100
+        ;   [0x0000302C] remainder = 0x00000000 (0)
+        ;       bytes  : 00 00 00 00
+        ;       binary : 00000000 00000000 00000000 00000000
         END
