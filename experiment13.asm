c0838c02b5ef444165024ee863c6128efd67efdd
kapiltrip <kapiltrip.kapil@gmail.com>
2025-11-11 15:33:02 +0530

    added some correction

--- experiment13.asm
+++ experiment13.asm
@@ -3,9 +3,10 @@
         ENTRY
 
 _start
-        LDR     R1, =IN_ADDR
+        LDR     R1, =0x00003100
         LDR     R0, [R1]
-        LDR     R2, =OUT_ADDR
+
+        LDR     R2, =0x00003104
         MOV     R3, #8
 
 loop_convert
@@ -24,7 +25,10 @@ loop_convert
 
 stop    B       stop
 
-        AREA    HEX_DATA, DATA, READONLY
-IN_ADDR DCD     0x00003100
-OUT_ADDR DCD    0x00003104
+; range: 0x00000000..0xFFFFFFFF (32-bit unsigned)
+; map: IN @ 0x00003100 (word), OUT @ 0x00003104 (9 bytes)
+; example1: IN=0x4AF23BCD -> OUT="4AF23BCD",0x00
+; example2: IN=0x00000005 -> OUT="00000005",0x00
+; write: put 32-bit input at 0x00003100 (as a word). After run,
+;        read 9 bytes from 0x00003104 (8 chars + 0x00 terminator).
         END
